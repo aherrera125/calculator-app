1f1edabf6f626a0b87a13751d287f71bbee1776b
aherrera125 <j.alejandroherrera@gmail.com>
2024-09-30 17:35:43 +0200

    fix the code and add operation sum

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,64 +1,93 @@
 import './App.css';
 import './styles/styles.css'
 import { useState, Fragment, useRef } from 'react';
-import Operation from './components/operations/operation'
 import Action from './components/actions/action'
 
 function App() {
   const [num, setNum] = useState();
-  let number = useRef();
-  var numA;
-  var numB;
+  const displayRef = useRef(null);
+  var firstNum;
+  var SecondNum;
+  var typeOp;
+  var result;
 
   const handleSelectNum = (selectedNum) => {
     setNum(selectedNum);
+  }
+
+  const handleTypeOp = (operation) => {
+    assignNumbers();
+    typeOp = operation;
   }
 
   const handleClear = () => {
     setNum("");
   }
 
-  const handleAdd = () => {
-    numA = parseFloat(number.current.value);
-  }
+  const assignNumbers=()=>{
+    !firstNum ? firstNum = parseFloat(displayRef.current.value) : firstNum = "";
+  }  
 
   const handleResult = () => {
-    numB = parseFloat(number.current.value);
-    setNum(numA+numB);
-    console.log(numA);
-    console.log(numB);
-    console.log(num);
+    SecondNum = parseFloat(displayRef.current.value);
+    switch (typeOp) {
+      case "add":
+        result = parseFloat(firstNum) + parseFloat(SecondNum);
+        setNum(result);
+        firstNum = "";
+        break;
+      case "less":
+        result = parseFloat(firstNum) - parseFloat(SecondNum);
+        setNum(result);
+        firstNum = "";
+        break;
+      case "mult":
+        result = parseFloat(firstNum) * parseFloat(SecondNum);
+        setNum(result);
+        firstNum = "";
+        break;
+      case "div":
+        if (parseFloat(SecondNum) !== 0) {
+          result = parseFloat(firstNum) / parseFloat(SecondNum);
+          setNum(result);
+          firstNum = "";
+        } else {
+          alert("Connot divide by zero!");
+          handleClear();
+        }
+        break;
+    }
   }
 
   return (
     <Fragment>
       <div className="border">
         <div>
-          <textarea readOnly value={num} ref={number}></textarea>
+          <textarea readOnly value={num} ref={displayRef}></textarea>
         </div>
         <div>
           <Action act="%"></Action>
           <button onClick={handleClear}>CE</button>
           <button onClick={handleClear}>C</button>
-          <Operation op="/" typeOp="div"></Operation>
+          <button onClick={handleTypeOp('div')}>/</button>
         </div>
         <div>
           <button onClick={() => handleSelectNum('7')}>7</button>
           <button onClick={() => handleSelectNum('8')}>8</button>
           <button onClick={() => handleSelectNum('9')}>9</button>
-          <Operation op="*" typeOp="mult"></Operation>
+          <button onClick={handleTypeOp('mult')}>*</button>
         </div>
         <div>
           <button onClick={() => handleSelectNum('4')}>4</button>
           <button onClick={() => handleSelectNum('5')}>5</button>
           <button onClick={() => handleSelectNum('6')}>6</button>
-          <Operation op="-" typeOp="less"></Operation>
+          <button onClick={handleTypeOp('less')}>-</button>
         </div>
         <div>
           <button onClick={() => handleSelectNum('1')}>1</button>
           <button onClick={() => handleSelectNum('2')}>2</button>
           <button onClick={() => handleSelectNum('3')}>3</button>
-          <button onClick={handleAdd}>+</button>
+          <button onClick={handleTypeOp('add')}>+</button>
         </div>
         <div>
           <Action act="+/-"></Action>
